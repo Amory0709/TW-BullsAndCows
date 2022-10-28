463265f9222f8a2d68c4f4be42fbbb1ab34ec228
Mengyu <mhan8@slb.com>
2022-10-28 14:54:07 +0800

    feat: can handle partial same now

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -1,20 +1,32 @@
 ﻿using System;
+using System.Linq;
 
 namespace BullsAndCows
 {
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private string secret;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            return "4A0B";
+            int counter = CountBulls(guess);
+
+            return $"{counter}A0B";
+        }
+
+        private int CountBulls(string guess)
+        {
+            var guessDigits = guess.Split(" ");
+            var secretDigits = secret.Split(" ");
+            return guessDigits.Where((t, i) => t == secretDigits[i]).Count();
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -7,15 +7,6 @@ namespace BullsAndCowsTest
     public class BullsAndCowsGameTest
     {
         [Fact]
-        public void Should_create_BullsAndCowsGame()
-        {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
-        }
-
-        [Fact]
         public void Should_return_4A0B_when_guess_given_guess_is_same_as_secret()
         {
             //given
@@ -26,6 +17,19 @@ namespace BullsAndCowsTest
             var res = game.Guess("1 2 3 4");
             //then
             Assert.Equal("4A0B", res);
+        }
+
+        [Fact]
+        public void Should_return_2A0B_when_guess_given_2_digits_of_guess_is_same_as_secret()
+        {
+            //given
+            var mockSecretGenerator = new Mock<SecretGenerator>();
+            mockSecretGenerator.Setup(x => x.GenerateSecret()).Returns("1 2 3 4");
+            var game = new BullsAndCowsGame(mockSecretGenerator.Object);
+            //when
+            var res = game.Guess("1 2 5 6");
+            //then
+            Assert.Equal("2A0B", res);
         }
     }
 }
